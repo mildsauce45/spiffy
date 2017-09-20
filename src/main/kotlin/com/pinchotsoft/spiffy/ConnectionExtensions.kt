107d83fcde3ffaa917879c3e2f779f4db43e2736
Jared Pinchot <jmpinchot@gmail.com>
2017-09-20 10:52:12 -0700

    cleaned up the connection extensions code some

--- src/main/kotlin/com/pinchotsoft/spiffy/ConnectionExtensions.kt
+++ src/main/kotlin/com/pinchotsoft/spiffy/ConnectionExtensions.kt
@@ -101,22 +101,109 @@ private fun insertMapValues(sql: String, parameters: Map<String, Any?>?): String
 }
 
 private fun <T> executeTextCommandWithResults(conn: Connection, sql: String, clazz: Class<T>): List<T> {
+    val results = Vector<T>()
+
+    executeTextCommand(conn, sql) {
+        while (it.next()) {
+            val m = mapModel(it, clazz) ?: continue
+
+            results.add(m)
+        }
+    }
+
+    return results
+}
+
+private fun <T> executeStoredProcWithResults(conn: Connection, sql: String, parameters: Map<String, Any?>, clazz: Class<T>): List<T> {
+    val results = Vector<T>()
+
+    executeStoredProcCommand(conn, sql, parameters) {
+        while (it.next()) {
+            val m = mapModel(it, clazz) ?: continue
+
+            results.add(m)
+        }
+    }
+
+    return results
+}
+
+private fun executeTextCommandWithResults(conn: Connection, sql: String): List<Map<String, Any?>> {
+    val results = Vector<Map<String, Any?>>()
+
+    executeTextCommand(conn, sql) {
+        val metadata = it.metaData
+        val columnNames = (1..metadata.columnCount).map { metadata.getColumnName(it) }
+
+        while (it.next()) {
+            val currMap = HashMap<String, Any?>()
+
+            for (name in columnNames)
+                currMap.put(name, it.getObject(name))
+
+            results.add(currMap)
+        }
+    }
+
+    return results
+}
+
+private fun executeStoredProcWithResults(conn: Connection, sql: String, parameters: Map<String, Any?>): List<Map<String, Any?>> {
+    val results = Vector<Map<String, Any?>>()
+
+    executeStoredProcCommand(conn, sql, parameters) {
+        val metadata = it.metaData
+        val columnNames = (1..metadata.columnCount).map { metadata.getColumnName(it) }
+
+        while (it.next()) {
+            val currMap = HashMap<String, Any?>()
+
+            for (name in columnNames)
+                currMap.put(name, it.getObject(name))
+
+            results.add(currMap)
+        }
+    }
+
+    return results
+}
+
+private fun executeTextCommand(conn: Connection, sql: String, mapper: (ResultSet) -> Unit) {
     var stmt: Statement? = null
     var rs: ResultSet? = null
 
     try {
-        stmt = conn.createStatement() ?: return emptyList()
-        rs = stmt.executeQuery(sql) ?: return emptyList()
+        stmt = conn.createStatement() ?: return
+        rs = stmt.executeQuery(sql) ?: return
 
-        val results = Vector<T>()
+        mapper(rs)
+    } catch (e: Exception) {
+        // Log something? Throw error?
+    } finally {
+        stmt?.close()
+        rs?.close()
+    }
+}
 
-        while (rs.next()) {
-            val m = mapModel(rs, clazz) ?: continue
+private fun executeStoredProcCommand(conn: Connection, sql: String, parameters: Map<String, Any?>, mapper: (ResultSet) -> Unit) {
+    var stmt: CallableStatement? = null
+    var rs: ResultSet? = null
 
-            results.add(m)
+    try {
+        val escapedSql = jdbcEscape(sql, parameters)
+
+        stmt = conn.prepareCall(escapedSql) ?: return
+
+        if (parameters.count() > 0) {
+            for ((key, value) in parameters)
+                stmt.setObject(key, value)
         }
 
-        return results
+        stmt.execute()
+
+        rs = stmt.resultSet ?: return
+
+        mapper(rs)
     } catch (e: Exception) {
         // Log something? Throw error?
     } finally {
@@ -124,121 +211,4 @@ private fun <T> executeTextCommandWithResults(conn: Connection, sql: String, cla
         rs?.close()
     }
 
-    return emptyList()
-}
-
-private fun <T> executeStoredProcWithResults(conn: Connection, sql: String, parameters: Map<String, Any?>, clazz: Class<T>): List<T> {
-    var stmt: CallableStatement? = null
-    var rs: ResultSet? = null
-
-    try {
-        val escapedSql = jdbcEscape(sql, parameters)
-
-        stmt = conn.prepareCall(escapedSql) ?: return emptyList()
-
-        if (parameters.count() > 0) {
-            for ((key, value) in parameters)
-                stmt.setObject(key, value)
-        }
-
-        stmt.execute()
-
-        rs = stmt.resultSet ?: return emptyList()
-
-        val results = Vector<T>()
-
-        while (rs.next()) {
-            val m = mapModel(rs, clazz) ?: continue
-
-            results.add(m)
-        }
-
-        return results
-    } catch (e: SQLException) {
-        println(e.message)
-        // Log something? Throw something?
-    } finally {
-        stmt?.close()
-        rs?.close()
-    }
-
-    return emptyList()
-}
-
-/// TODO: I don't like this, but I can't seem to make the types work properly to call into the Class<T> version
-private fun executeTextCommandWithResults(conn: Connection, sql: String): List<Map<String, Any?>> {
-    var stmt: Statement? = null
-    var rs: ResultSet? = null
-
-    try {
-        stmt = conn.createStatement() ?: return emptyList()
-        rs = stmt.executeQuery(sql) ?: return emptyList()
-
-        val metadata = rs.metaData
-        val columnNames = (1..metadata.columnCount).map { metadata.getColumnName(it) }
-
-        val results = Vector<Map<String, Any?>>()
-
-        while (rs.next()) {
-            val currMap = HashMap<String, Any?>()
-
-            for (name in columnNames)
-                currMap.put(name, rs.getObject(name))
-
-            results.add(currMap)
-        }
-
-        return results
-    } catch (e: Exception) {
-
-    } finally {
-        stmt?.close()
-        rs?.close()
-    }
-
-    return emptyList()
-}
-
-private fun executeStoredProcWithResults(conn: Connection, sql: String, parameters: Map<String, Any?>): List<Map<String, Any?>> {
-    var stmt: CallableStatement? = null
-    var rs: ResultSet? = null
-
-    try {
-        val escapedSql = jdbcEscape(sql, parameters)
-
-        stmt = conn.prepareCall(escapedSql) ?: return emptyList()
-
-        if (parameters.count() > 0) {
-            for ((key, value) in parameters)
-                stmt.setObject(key, value)
-        }
-
-        stmt.execute()
-
-        rs = stmt.resultSet ?: return emptyList()
-
-        val metadata = rs.metaData
-        val columnNames = (1..metadata.columnCount).map { metadata.getColumnName(it) }
-
-        val results = Vector<Map<String, Any?>>()
-
-        while (rs.next()) {
-            val currMap = HashMap<String, Any?>()
-
-            for (name in columnNames)
-                currMap.put(name, rs.getObject(name))
-
-            results.add(currMap)
-        }
-
-        return results
-    } catch (e: SQLException) {
-        println(e.message)
-        // Log something? Throw something?
-    } finally {
-        stmt?.close()
-        rs?.close()
-    }
-
-    return emptyList()
 }
